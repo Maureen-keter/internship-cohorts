ce9c81ee859d6d7a6961bda538cbcf07eaad1660
maureen <maureen.chelangat@student.moringaschool.com>
2025-09-19 21:05:20 +0300

    create cohorts table

--- interns.sql
+++ interns.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS grad_rotation_db;
 CREATE DATABASE grad_rotation_db;
 USE grad_rotation_db;
 
+-- cohorts
+CREATE TABLE cohorts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  start_date DATE NOT NULL,
+  end_date DATE DEFAULT NULL,
+  description TEXT
+);
+
